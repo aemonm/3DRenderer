6dae34023eb3a02fc3c918e98d5784f1d2afaf68
Aemon Malone <aemon@fastmail.com>
2022-04-02 18:26:44 -0400

    setup SDL window and renderer

--- src/main.c
+++ src/main.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
 
+SDL_Window *window;
+SDL_Renderer *renderer;
+bool is_running = false;
+bool initialize_window(void)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        fprintf(stderr, "Error Initializing SDL\n");
+        return false;
+    }
+    window = SDL_CreateWindow(
+        NULL,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        400,
+        600,
+        SDL_WINDOW_BORDERLESS);
 
-int main(void) {
-    printf("Hello, world\n");
+    if (!window)
+    {
+        fprintf(stderr, "Error creating window\n");
+        return false;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, 0);
+
+    if (!renderer)
+    {
+        fprintf(stderr, "Error creating renderer\n");
+    }
+
+    return true;
+}
+
+int main(void)
+{
+    is_running = initialize_window();
     return 0;
-
 }
