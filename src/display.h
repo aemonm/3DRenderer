0f1f3b0b858a6ce32b4cd16bd05615ec1cbc7889
Aemon Malone <aemon@fastmail.com>
2022-05-03 21:38:29 -0400

    implement dynamic arrays

--- src/display.h
+++ src/display.h
@@ -34,4 +34,6 @@ void destroy_window(void);
 
 void draw_line(int x0, int y0, int x1, int y1, uint32_t color);
 
+void draw_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color);
+
 #endif
--- src/main.c
+++ src/main.c
@@ -6,8 +6,9 @@
 #include "vector.h"
 #include "mesh.h"
 #include "triangle.h"
+#include "darray.h"
 
-triangle_t triangles_to_render[N_MESH_FACES];
+triangle_t* triangles_to_render = NULL;
 
 bool is_running = false;
 int previous_frame_time = 0;
@@ -58,6 +59,7 @@ void update(void)
         SDL_Delay(time_to_wait);
     }
     previous_frame_time = SDL_GetTicks64();
+    triangles_to_render = NULL;
     cube_rotation.x += 0.001;
     cube_rotation.y += 0.001;
     cube_rotation.z += 0.001;
@@ -85,29 +87,23 @@ void update(void)
 
             projected_triangle.points[j] = projected_point;
         }
-        triangles_to_render[i] = projected_triangle;
+        array_push(triangles_to_render, projected_triangle);
     }
 }
 
 void render(void)
 {
-    // draw_grid();
-    for (int i = 0; i < N_MESH_FACES; i++)
+    int triange_count = array_length(triangles_to_render);
+    for (int i = 0; i < triange_count; i++)
     {
         triangle_t triangle = triangles_to_render[i];
 
-        // render vertices;
-        for (int j = 0; j < 3; j++)
-        {
-            draw_rect(triangle.points[j].x, triangle.points[j].y, 3, 3, 0xffffff00);
-        }
-        // render lines
-        for (int j = 0; j < 3; j++)
-        {
-            vect2_t next_points = j == 2 ? triangle.points[0] : triangle.points[j + 1];
-            draw_line(triangle.points[j].x, triangle.points[j].y, next_points.x, next_points.y, 0xffffff00);
-        }
+        draw_rect(triangle.points[0].x, triangle.points[0].y, 3, 3, 0xffffff00);
+        draw_rect(triangle.points[1].x, triangle.points[1].y, 3, 3, 0xffffff00);
+        draw_rect(triangle.points[2].x, triangle.points[2].y, 3, 3, 0xffffff00);
+        draw_triangle(triangle.points[0].x, triangle.points[0].y, triangle.points[1].x, triangle.points[1].y, triangle.points[2].x, triangle.points[2].y, 0xffffff00);
     }
+    array_free(triangles_to_render);
     render_color_buffer();
     clear_color_buffer(0xFF111111);
     SDL_RenderPresent(renderer);
