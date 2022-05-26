02cb655c4d6c6218c5bb4c498304ad7d3c7854af
Aemon Malone <aemon@fastmail.com>
2022-05-26 13:01:30 -0400

    add initial vector operation functions

--- src/vector.c
+++ src/vector.c
@@ -1,6 +1,42 @@
 #include <math.h>
 #include "vector.h"
 
+// Vector 2D Functions
+float get_vect2_length(vect2_t v)
+{
+    return sqrt(v.x * v.x + v.y * v.y);
+}
+
+vect2_t add_vect2(vect2_t v1, vect2_t v2)
+{
+    vect2_t v_new = {.x = v1.x + v2.x, .y = v1.y + v2.y};
+    return v_new;
+}
+
+vect2_t sub_vect2(vect2_t v1, vect2_t v2)
+{
+    vect2_t v_new = {.x = v1.x - v2.x, .y = v1.y - v2.y};
+    return v_new;
+}
+
+// Vector 3D Functions
+float get_vect3_length(vect3_t v)
+{
+    return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+vect3_t add_vect3(vect3_t v1, vect3_t v2)
+{
+    vect3_t v_new = {.x = v1.x + v2.x, .y = v1.y + v2.y, .z = v1.z + v1.z};
+    return v_new;
+}
+
+vect3_t sub_vect3(vect3_t v1, vect3_t v2)
+{
+    vect3_t v_new = {.x = v1.x - v2.x, .y = v1.y - v2.y, .z = v1.z - v1.z};
+    return v_new;
+}
+
 vect3_t vec3_rotate_x(vect3_t v, float angle)
 {
     vect3_t rotated_vector = {
--- src/vector.h
+++ src/vector.h
@@ -14,6 +14,15 @@ typedef struct
     float z;
 } vect3_t;
 
+float get_vect2_length(vect2_t v);
+float get_vect3_length(vect3_t v);
+
+vect2_t add_vect2(vect2_t v1, vect2_t v2);
+vect3_t add_vect3(vect3_t v1, vect3_t v2);
+
+vect2_t sub_vect2(vect2_t v1, vect2_t v2);
+vect3_t sub_vect3(vect3_t v1, vect3_t v2);
+
 vect3_t vec3_rotate_x(vect3_t vec, float angle);
 vect3_t vec3_rotate_y(vect3_t vec, float angle);
 vect3_t vec3_rotate_z(vect3_t vec, float angle);
